781f5158236c790a1b1db24c8149e0351e2d8a6f
Michael-Kochis <rhishisikk@gmail.com>
2021-04-23 18:58:55 -0500

    Corrected spelling error on login screen

--- src/js/Login.js
+++ src/js/Login.js
@@ -21,7 +21,7 @@ function Login() {
                 await login(emailRef.current.value, passwordRef.current.value);
                 history.push("/");
             } catch {
-                setError("Failed to create an account");
+                setError("Failed to log in.");
             }
         
         setLoading(false)
