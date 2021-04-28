acc5a5ca4edb61a7397d322bce27e333d027b338
Michael-Kochis <rhishisikk@gmail.com>
2021-04-27 21:28:43 -0500

    Now loading merchants.

---
+++ src/js/context/useMerchant.js
@@ -0,0 +1,52 @@
+import  {React, useEffect, useState } from 'react'
+import {Button} from 'react-bootstrap'
+import {database} from '../firebase'
+import {useAuth} from './authContext'
+
+function UseMerchant() {
+  const [children, setChildren] = useState([]);
+  const { currentUser } = useAuth();
+
+  function doNothing(object, e) {
+    e.preventDefault();
+    console.log(object);
+  }
+
+  async function getChildren() {
+    try {
+        await database.persona
+        .doc(`${currentUser.uid}`)
+        .collection("Games")
+        .get().then( snapShot => { 
+            setChildren(snapShot.docs.map(doc => doc.data()))
+        }).catch(error => alert(error));
+        
+        console.log(children);
+        return children;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+      getChildren();
+  }, []) 
+  
+  return (
+    <div>
+      {children && children.length > 0 && (
+        <div className="d-flex flex-wrap">
+          {children.map((child) => (
+            <Button id={child.name} onClick={(e) => doNothing(child.name, e)}>
+              {child.name}
+            </Button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export {
+    UseMerchant
+}
