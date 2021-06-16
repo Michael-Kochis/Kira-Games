8ea58f33c29df6793709a5010e4247f5e27ce4dc
Michael-Kochis <rhishisikk@gmail.com>
2021-06-16 00:25:35 -0500

    Worker icon changes

--- src/js/repair-merchant/merchant-icon.js
+++ src/js/repair-merchant/merchant-icon.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faToolbox} from '@fortawesome/free-solid-svg-icons'
 
 import { connect } from 'react-redux'
 
 function MerchantIcon(props) {
     const currentMerchant = props.merchant;
+    const [hover, setHover] = useState(false);
 
     const dragEnd = (event) => {
         const target = event.target;
@@ -21,16 +25,26 @@ function MerchantIcon(props) {
         event.stopPropagation();
     }
 
-    return (
+    const onHover = () => {
+        setHover(true);
+      };
+    
+      const onLeave = () => {
+        setHover(false);
+      };
+      
+      return (
         <div
             id={currentMerchant.name}
             onDragEnd={dragEnd}
             onDragStart={dragStart}
             onDragOver={dragOver}
+            onMouseEnter={onHover}
+            onMouseLeave={onLeave}
             draggable="true"
             className="merchant worker"
         >
-            {currentMerchant.name}
+            {hover?currentMerchant.name:<FontAwesomeIcon icon={faToolbox} />}
         </div>
     )
 }
